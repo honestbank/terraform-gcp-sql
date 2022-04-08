7962d9f0ece9de3012fd7899d035186e73a14170
Phathaisarn Chalermchuang <phathaisarn@honestbank.com>
2022-04-08 13:41:19 +0700

    fix: pin wrong format for required providers (#6)

--- modules/google_sql_database_instance/main.tf
+++ modules/google_sql_database_instance/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.0.0"
+      version = "~> 4.0"
     }
   }
 }
@@ -30,9 +30,11 @@ resource "google_sql_database_instance" "instance" {
     }
 
     ip_configuration {
+      #tfsec:ignore:google-sql-encrypt-in-transit-data
       require_ssl = var.settings_ip_configuration_require_ssl
 
       #checkov:skip=CKV_GCP_60:Ensure Cloud SQL database does not have public IP - default value is false
+      #tfsec:ignore:google-sql-no-public-access
       ipv4_enabled = var.settings_ip_configuration_ipv4_enabled
 
       private_network = var.settings_ip_configuration_private_network
@@ -62,25 +64,28 @@ resource "google_sql_database_instance" "read_replica" {
 
   settings {
 
-    tier                  = var.read_replica_settings_tier
-    disk_size             = var.settings_disk_size
-    disk_autoresize       = var.settings_disk_autoresize
-    disk_type             = var.settings_disk_type
+    tier            = var.read_replica_settings_tier
+    disk_size       = var.settings_disk_size
+    disk_autoresize = var.settings_disk_autoresize
+    disk_type       = var.settings_disk_type
 
     # Not supported for Read Replica
-    availability_type     = "ZONAL"
+    availability_type = "ZONAL"
 
     disk_autoresize_limit = var.settings_disk_autoresize_limit
 
     backup_configuration {
+      #tfsec:ignore:google-sql-enable-backup:read replica no need to backup
       enabled            = false
       binary_log_enabled = false
     }
 
     ip_configuration {
+      #tfsec:ignore:google-sql-encrypt-in-transit-data:because default value is true
       require_ssl = var.settings_ip_configuration_require_ssl
 
       #checkov:skip=CKV_GCP_60:Ensure Cloud SQL database does not have public IP - default value is false
+      #tfsec:ignore:google-sql-no-public-access
       ipv4_enabled = var.read_replica_settings_ip_configuration_ipv4_enabled
 
       private_network = var.settings_ip_configuration_private_network
