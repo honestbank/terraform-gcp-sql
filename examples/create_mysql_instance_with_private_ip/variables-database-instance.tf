2102f696f6e99daf8b2f6ed53af8b4920869415b
Bibek Rauniyar <bibek@honestbank.com>
2024-07-08 14:15:15 +0700

    feat: DEVOP-4544| Remove the deprecated attributes from terraform-gcp-sql component module

--- examples/create_mysql_instance_with_private_ip/variables-database-instance.tf
+++ examples/create_mysql_instance_with_private_ip/variables-database-instance.tf
@@ -44,8 +44,8 @@ variable "settings_backup_configuration_binary_log_enabled" {
   default     = true
 }
 
-variable "settings_ip_configuration_require_ssl" {
-  description = "(Optional) Whether SSL connections over IP are enforced or not."
-  type        = bool
-  default     = true
+variable "settings_ip_configuration_ssl_mode" {
+  description = " (Optional) Specify how SSL connection should be enforced in DB connections."
+  type        = string
+  default     = "ALLOW_UNENCRYPTED_AND_ENCRYPTED"
 }
--- modules/google_service_networking_connection/variables.tf
+++ modules/google_service_networking_connection/variables.tf
@@ -15,7 +15,7 @@ variable "reserved_peering_ranges" {
 }
 
 variable "deletion_policy" {
-  description = "(Optional) The deletion policy for the peering. One of 'DELETE' or 'ABANDON'. Defaults to 'DELETE'."
+  description = "(Optional) The deletion policy for the service networking connection. Setting to ABANDON allows the resource to be abandoned rather than deleted. This will enable a successful terraform destroy when destroying CloudSQL instances. Use with care as it can lead to dangling resources."
   type        = string
   default     = ""
 }
